63b4e391e4974fe6a324c3f0f83cc053960b0943
Girish Kumar . S <56218745+GirishKumar-technogeek@users.noreply.github.com>
2021-05-16 08:54:16 +0530

    Update custom_azure.py

--- foodfinds/custom_azure.py
+++ foodfinds/custom_azure.py
@@ -1,7 +1,7 @@
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'foodfinds' # Must be replaced by your <storage_account_name>
-    account_key = '4/FNPwIaeKfW11VguGacYw5emNxLy5/MjwP8Tg+Hc4cHo7ooov16t42x+A30Gj1YhsOpZjYqV9gv9YjpIAzMiw==' # Must be replaced by your <storage_account_key>
+    account_name = 'account-name' # Must be replaced by your <storage_account_name>
+    account_key = 'account-key' # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
